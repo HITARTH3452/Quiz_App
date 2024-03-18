24407b6b846dddf4a6b7e28999c66e9b0e72ef09
HITARTH3452 <hitarthsingh2201@gmail.com>
2024-03-18 11:54:12 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,82 @@
+export const data = [
+    {
+        question: "Which of the following is used in React.js to increase performance?",
+        option1: "Virtual Dom",
+        option2: "Original Dom",
+        option3: "Both A and B",
+        option4: "None of the above",
+        ans: 1
+    },
+    {
+        question: "What is ReactJS?",
+        option1: "Server-side framework",
+        option2:"user interface framework",
+        option3:"both a and b",
+        option4:"none of the above",
+        ans: 2
+    },
+    {
+        question: "Identify the one which is used to pass data to components fro outside",
+        option1:"Render with arguments",
+        option2:"setState",
+        option3:"PropTypes",
+        option4:"props",
+        ans: 4
+    },
+    {
+        question: "Who created React.js",
+        option1:"Jordan Mike",
+        option2:"Jordan Walke",
+        option3:"Tim Lee",
+        option4:"Jordan Lee",
+        ans: 2
+    },
+    {
+        question: "In which language is React.js written",
+        option1:"Python",
+        option2:"JavaScript",
+        option3:"Java",
+        option4:"PHP",
+        ans: 2
+    },
+    {
+        question: "What is babel?",
+        option1:"JavaScript compiler",
+        option2:"Javascript interpreter",
+        option3:"JavaScript transpiler",
+        option4:"None of the above",
+        ans: 1
+    },
+    {
+        question: "Identify the commond used to create a react app.",
+        option1:"npm install create-react-app",
+        option2:"npm install -g create-react-app",
+        option3:"install -g create-react-app",
+        option4:"None of the above",
+        ans: 2
+    },
+    {
+        question: "Which of the following port is the default where webpack-dev-server runs?",
+        option1:"3000",
+        option2:"3306",
+        option3:"3030",
+        option4:"8080",
+        ans: 4
+    },
+    {
+        question: "How many elements can a valid react component return?",
+        option1:"1",
+        option2:"2",
+        option3:"3",
+        option4:"4",
+        ans: 1
+    },
+    {
+        question: "A state in React.js is also known as?",
+        option1:"The internet storage of the component",
+        option2:"External storage of the component",
+        option3:"Permanent storage",
+        option4:"All of the above",
+        ans: 1
+    },
+]
